71238f2b5d53e958cc51ffe600444095e1a88ba8
Tomek Rybczyński <tomek.rybka@gmail.com>
2018-03-06 19:26:51 +0100

    Wrap components withRouter method outside component definition

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,22 @@
-import React                            from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import QuestionsList                    from './containers/QuestionsList';
-import QuestionDetails                  from './containers/QuestionDetails';
+import React           from 'react';
+import QuestionsList   from './containers/QuestionsList';
+import QuestionDetails from './containers/QuestionDetails';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom';
+
+const RQuestionsList   = withRouter(QuestionsList);
+const RQuestionDetails = withRouter(QuestionDetails);
 
 export default () => {
   return (
    <BrowserRouter>
      <Switch>
-       <Route exact path='/' component={QuestionsList}/>
-       <Route path='/:id' component={QuestionDetails}/>
+       <Route exact path='/' component={RQuestionsList}/>
+       <Route path='/:id' component={RQuestionDetails}/>
      </Switch>
    </BrowserRouter>
   )
